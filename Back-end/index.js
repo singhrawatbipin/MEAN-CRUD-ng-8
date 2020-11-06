f8876bf8ee54f6865d3bb1c6879b9046409d223c
Bipin Singh Rawat <singhrawatbipin@gmail.com>
2020-11-06 12:32:01 +0530

    updated verison working good

--- Back-end/index.js
+++ Back-end/index.js
@@ -7,14 +7,15 @@ var registrationController = require('./controllers/registrationController.js');
 var quotationController = require('./controllers/quotationController')
 var app = express();
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*'); //<-- you can change this with a specific url like http://localhost:4200
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'http://localhost:4200'); //<-- you can change this with a specific url like http://localhost:4200
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    //res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
     next();
- });
- 
+});
+
 app.listen(3000, () => console.log("Express server started at port number : 3000"));
 
 app.use('/registrations', registrationController);
